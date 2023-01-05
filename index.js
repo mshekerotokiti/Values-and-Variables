2c2aedd80f8ae75983b47ff9c0ef017728b9e8d4
mshekerotokiti <sharontokiti@gmail.com>
2023-01-05 09:41:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//let js = "amazing";
+        //if (js === "amazing") alert ("Javascript is Fun!");
+
+        console. log(20*4-30);
+        console. log( "Benson is the Manager at Kurunzi Media");
+
+        let firstName = "Sharon";
+        console. log( firstName);
+
+        let country = "Kenya";
+        let continent = "Africa";
+        let population = "100 million";
+
+        console. log (country );
+        console. log (continent);
+        console. log (population);
+        
